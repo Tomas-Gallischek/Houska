14ee338f88ab0c2c4272a92f2448094d28034952
Tomas Gallischek <t.gall@seznam.cz>
2025-08-26 00:47:04 +0200

    autosave

--- houska/hracapp/forms.py
+++ houska/hracapp/forms.py
@@ -3,11 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class RegistrationForm(UserCreationForm):
-    username = forms.CharField(label="Uživatelské jméno", max_length=150, required=True)
+    username = forms.CharField(label="Jméno_Příjmení", max_length=150, required=True)
     email = forms.EmailField(label="Email", required=True)
     password1 = forms.CharField(label="Heslo", widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(label="Potvrzení hesla", widget=forms.PasswordInput, required=True)
+    GENDER_CHOICES = (
+        ('male', 'Muž'),
+        ('female', 'Žena'),
+        ('other', 'Jiné'),
+    )
+    gender = forms.ChoiceField(label="Pohlaví", choices=GENDER_CHOICES, required=False)
+    orders = forms.IntegerField(label="Objednávky", required=False)
+    nickname = forms.CharField(label="Přezdívka", max_length=30, required=False)
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('gender',)
+        fields = UserCreationForm.Meta.fields + ('gender', 'orders', 'nickname')
--- houska/hracapp/models.py
+++ houska/hracapp/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    gender = models.CharField(max_length=100, blank=True, null=True)
+    GENDER_CHOICES = (
+        ('male', 'Muž'),
+        ('female', 'Žena'),
+        ('other', 'Jiné'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    orders = models.IntegerField(("Počet objednávek"), blank=True, null=True)
+    nickname = models.CharField(max_length=30, blank=True, null=True)
+    pozn = models.TextField(("Poznámka"), blank=True, null=True)
 
     def __str__(self):
         return self.username
+
+#POKUD ZDE PŘIDÁŠ NOVÝ MODEL, JE TŘEBA JEJ PŘIDAT I D OREGISTRAČNÍHO FORMULÁŘE

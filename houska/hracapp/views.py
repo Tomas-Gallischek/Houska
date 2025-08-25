b6cb3f2c460f04d3aa23f703236142173d3137b4
Tomas Gallischek <t.gall@seznam.cz>
2025-08-25 22:17:01 +0200

    autosave

--- houska/hracapp/views.py
+++ houska/hracapp/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 def index(request):
     return render(request, 'hracapp/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'hracapp/register.html', {'form': form})

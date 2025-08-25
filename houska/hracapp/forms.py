0a69d746339caba53449ecda8062bebaba41363a
Tomas Gallischek <t.gall@seznam.cz>
2025-08-25 23:30:15 +0200

    autosave

--- houska/hracapp/forms.py
+++ houska/hracapp/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(label="Uživatelské jméno", max_length=150, required=True)
+    email = forms.EmailField(label="Email", required=True)
+    password1 = forms.CharField(label="Heslo", widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label="Potvrzení hesla", widget=forms.PasswordInput, required=True)
 
     class Meta:
         model = User
--- houska/hracapp/views.py
+++ houska/hracapp/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'hracapp/index.html')
 
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login-url')
     else:
-        form = CustomUserCreationForm()
+        form = RegistrationForm()
     return render(request, 'hracapp/register.html', {'form': form})
 
+
+@login_required
 def profile(request):
     return render(request, 'hracapp/profile.html')
 
+@login_required
 def protected_page(request):
     return render(request, 'hracapp/protected-page.html')

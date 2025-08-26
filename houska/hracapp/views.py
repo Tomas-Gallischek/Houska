62e7b95cbe1e0dda695f60728edd919f08c899ef
Tomas Gallischek <t.gall@seznam.cz>
2025-08-26 10:23:45 +0200

    autosave

--- houska/hracapp/views.py
+++ houska/hracapp/views.py
@@ -25,6 +25,20 @@ def profile(request):
         request.user.orders = 1
         request.user.save()
 
+    if request.method == 'POST':
+        new_orders = int(request.POST.get('orders'))
+        if new_orders is not None:
+            if new_orders >= request.user.orders:
+                request.user.orders = new_orders
+                request.user.save()
+                lower_error = 1
+            else:
+                lower_error = 2
+        else:
+            lower_error = 3
+    if lower_error is None:
+        lower_error = 0
+        
     XP_aktual = request.user.orders
     lvl_aktual = 1
 
@@ -43,7 +57,9 @@ def profile(request):
         'XP_aktual': XP_aktual,
         'lvl_aktual': lvl_aktual,
         'lvl_next': lvl_next,
-        'XP_potrebne_next': XP_potrebne_next
+        'XP_potrebne_next': XP_potrebne_next,
+        'lower_error': lower_error
+
     })
 
 @login_required

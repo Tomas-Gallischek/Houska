c090bca15da6ea78f4b31494de531d1ebaa8b43b
Tomas Gallischek <t.gall@seznam.cz>
2025-08-30 14:21:05 +0200

    autosave

--- houska/shopapp/views.py
+++ houska/shopapp/views.py
@@ -1,14 +1,8 @@
-import json
-from urllib import request
-from django import utils
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
-from django.contrib import messages
-from django.utils import timezone
-from django.http import JsonResponse
 from hracapp import views
 from itemsapp import weapons
+from django.shortcuts import render
+from urllib import request
+from django import utils
 
 user = views.profile(request)
-result = weapons.weapons_generator(user)
+
